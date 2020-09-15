722e729f84ab5c562d10157ad87d343e6b42d6bb
milahu <milahu@gmail.com>
2020-09-15 18:42:53 +0200

    up

--- test.sh
+++ test.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# this will pass
+config='rollup.config.js'
+moved_1='rollup.config.MOVED.js'
+moved_2='rollup.config.js.MOVED'
+
+# pass
+echo test no symlink
 rollup -c
 
-# this will fail
-orig='rollup.config.js'
-moved='rollup.config.js.MOVED'
-mv $orig $moved
-ln -s $moved $orig # create symlink to config file
+# fail
+echo; echo test symlink $moved_1
+mv $config $moved_1; ln -s $moved_1 $config
 rollup -c
-# error: [!] SyntaxError: Cannot use import statement outside a module
-# [ the symlink 'rollup.config.js' is not recognized as js module file ]
+rm $config; mv $moved_1 $config
 
-# restore old state
-rm $orig
-mv $moved $orig
+# fail
+echo; echo test symlink $moved_2
+mv $config $moved_2; ln -s $moved_2 $config
+rollup -c
+rm $config; mv $moved_2 $config
+
+# pass
+echo; echo test symlink $moved_1 + require
+mv $config $moved_1; ln -s $moved_1 $config
+sed -i "s/import fs from 'fs';/const fs = require('fs');/" $config
+rollup -c
+sed -i "s/const fs = require('fs');/import fs from 'fs';/" $config
+rm $config; mv $moved_1 $config
