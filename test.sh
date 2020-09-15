4c154673ea17228583e61c1d67961f70eeacc17b
milahu <milahu@gmail.com>
2020-09-15 18:24:35 +0200

    up

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ moved='rollup.config.js.MOVED'
 mv $orig $moved
 ln -s $moved $orig # create symlink to config file
 rollup -c
+# error: [!] SyntaxError: Cannot use import statement outside a module
+# [ the symlink 'rollup.config.js' is not recognized as js module file ]
 
 # restore old state
 rm $orig
